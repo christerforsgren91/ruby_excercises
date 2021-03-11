6043b5ef7ef0a1ffc845b35ce74d883e229e8799
Christer <christerforsgren91@gmail.com>
2021-03-11 14:55:30 +0100

    First ruby code

---
+++ my_group.rb
@@ -0,0 +1,4 @@
+person_1 = {name: "Christer", gender: "Man", age: 29}
+person_2 = {name: "Lisa", gender: "Woman", age: 26}
+person_3 = {name: "Janne", gender: "Man", age: 55}
+my_group = [person_1, person_2, person_3]
