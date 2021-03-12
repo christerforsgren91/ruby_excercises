3369d970da54a5232fc62a65fa28f912bb509c29
Christer <christerforsgren91@gmail.com>
2021-03-12 15:27:32 +0100

    array, hash, key

--- my_group.rb
+++ my_group.rb
@@ -1,4 +1,9 @@
+my_group = []
 person_1 = {name: "Christer", gender: "Man", age: 29}
 person_2 = {name: "Lisa", gender: "Woman", age: 26}
 person_3 = {name: "Janne", gender: "Man", age: 55}
+
 my_group = [person_1, person_2, person_3]
+my_group.each do |person|
+    puts "#{person[:name]} is a #{person[:age]} years old #{person[:gender]}."
+end
